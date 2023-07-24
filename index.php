2f04be3342d5d3ef489f2adbfd83ce076fe35e5b
Ka-vo <psadaski@gmail.com>
2023-07-24 22:10:16 +0300

    init

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+$example_persons_array = [
+  [
+    'fullname' => 'Иванов Иван Иванович',
+    'job' => 'tester',
+  ],
+  [
+    'fullname' => 'Степанова Наталья Степановна',
+    'job' => 'frontend-developer',
+  ],
+  [
+    'fullname' => 'Пащенко Владимир Александрович',
+    'job' => 'analyst',
+  ],
+  [
+    'fullname' => 'Громов Александр Иванович',
+    'job' => 'fullstack-developer',
+  ],
+  [
+    'fullname' => 'Славин Семён Сергеевич',
+    'job' => 'analyst',
+  ],
+  [
+    'fullname' => 'Цой Владимир Антонович',
+    'job' => 'frontend-developer',
+  ],
+  [
+    'fullname' => 'Быстрая Юлия Сергеевна',
+    'job' => 'PR-manager',
+  ],
+  [
+    'fullname' => 'Шматко Антонина Сергеевна',
+    'job' => 'HR-manager',
+  ],
+  [
+    'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+    'job' => 'analyst',
+  ],
+  [
+    'fullname' => 'Бардо Жаклин Фёдоровна',
+    'job' => 'android-developer',
+  ],
+  [
+    'fullname' => 'Шварцнегер Арнольд Густавович',
+    'job' => 'babysitter',
+  ],
+];
+
+function getFullnameFromParts($surname, $name, $patronomyc)
+{
+  if ((is_string($surname))  && (is_string($name)) && (is_string($patronomyc))) {
+    return $surname . " " . $name . " " . $patronomyc;
+  } else {
+    return 'УВЫ';
+  }
+}
+
+echo getFullnameFromParts('Иваныч', 5, 'Иваныч');

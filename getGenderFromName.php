7e5d08e8b15516c9c2fe2a367b88690a00f458f9
Ka-vo <psadaski@gmail.com>
2023-07-31 10:28:14 +0300

    change gender function

--- getGenderFromName.php
+++ getGenderFromName.php
@@ -1,75 +1,39 @@
 <?php
 require "getPartsFromFullname.php";
 
-function getGenderFromName($fullNameString){
-    $array  = getPartsFromFullname($fullNameString);  
-    $gender = 0;
-    
-     foreach ($array as $key => $value) {
-      $lastLetterMen = mb_substr($value, -1);
-      $lastLetterWoman = mb_substr($value, -2);
-      if($key == 'surname' && $lastLetterMen == "в") {
-         $gender = $gender +  1;
-     } elseif($key = 'surname' && $lastLetterWoman = "ва") {
-        $gender = $gender  - 1;
-     }
-    
-   if ($gender >= 1){
-    return 'мужской';
+function getGenderFromName($fullNameString)
+{
+   $array  = getPartsFromFullname($fullNameString);
+   $gender = 0;
+   $name = $array['name'];
+   if (mb_substr($name, -1) == "а") {
+      $gender = $gender - 1;
+   } elseif (mb_substr($name, -1) == "н" || mb_substr($name, -1) == "й") {
+      $gender = $gender + 1;
    }
-     
-   return  $gender ;
+
+   $surname =  $array['surname'];
+   if (mb_substr($surname, -2) == "ва") {
+      $gender = $gender - 1;
+   } elseif (mb_substr($surname, -1) == "в") {
+      $gender = $gender  + 1;
+   }
+
+   $patronomyc =  $array['patronomyc'];
+   if (mb_substr($patronomyc, -3) == "вна") {
+      $gender = $gender - 1;
+   } elseif (mb_substr($patronomyc, -2) == "ич") {
+      $gender = $gender  + 1;
+   }
+
+   $total = ($gender <=> 0);
+   if ($total == 1) {
+      return "Мужской пол";
+   } elseif ($total == -1) {
+      return "Женский пол";
+   } else {
+      return  "Неопределенный пол";
+   }
 }
-}
-echo getGenderFromName('Иванова Иван Иванович');
 
-
-// function getGenderFromName($fullNameString){
-//     $array  = getPartsFromFullname($fullNameString);  
-//     $gender = 0;
-    
-
-//     $name = $array['name'];
-//     if (mb_substr($name, -1) == "а"){
-//       $gender = $gender -1;  
-//     }elseif(mb_substr($name, -1) == "н" || mb_substr($name, -1) == "й"){
-//        $gender = $gender + 1 ; 
-//     }
-
-//     $surname =  $array['surname'];
-//     if(mb_substr($name, -1) == "ва") {
-//           $gender = $gender - 1 ;
-//       } elseif(mb_substr($name, -1) == "в") {
-//          $gender = $gender  +1;
-//       }
-//     //  foreach ($array as $key => $value) {
-//     //   $lastLetterMen = mb_substr($value, -1);
-//     //   $lastLetterWoman = mb_substr($value, -2);
-//     //   if($key == 'surname' && $lastLetterMen == "в") {
-//     //      $gender = $gender + 1 ;
-//     //  } elseif($key == 'surname' && $lastLetterWoman = "ва") {
-//     //     $gender = $gender  -1;
-//     //  }
-
-//     //  if($key == 'name' && ($key == 'surname' && (mb_substr($value, -1) == "й" || mb_substr($value, -1) == "н")) ) {
-//     //      $gender = $gender + 1 ;
-//     //  } elseif($key == 'surname' && mb_substr($value, -1) == "а") {
-//     //     $gender = $gender  -1;
-//     //  }
-
-//     return $gender;
-
-// //     if ($gender >= 1){
-// //     return 'мужской';
-// //    } else{
-// //      return 'женский';  
-// //    }
-     
-   
-   
-     
-//    //return  $gender;
-//   // return $lastLetterMen;
-
-// }
-// echo getGenderFromName('Иванов Иван Иванович');
+echo getGenderFromName('Иваново Ивар Ивановч');
